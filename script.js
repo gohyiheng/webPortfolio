3cd69e145c24a729d3251615211e90d6435ff2d5
yihenggoh <yiheng29@gmail.com>
2024-06-29 21:20:11 +0800

    simple popup button to show content

---
+++ script.js
@@ -0,0 +1,8 @@
+function popupFunc(){
+    const popup = document.querySelector(".pop-up");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
