e532761ec5f0c7b39018f8646aa53742f0409734
gohyiheng <yiheng29@gmail.com>
2025-06-09 15:56:41 +1000

    new: added page for detailed project

---
+++ src/pages/aboutProjects.tsx
@@ -0,0 +1,12 @@
+import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+
+export default function detailedProjects(){
+    return (
+        <>
+            <Navbar />
+            <div className="text-center text-2xl max-w-5xl items-center mx-auto">TBC</div>
+            <Contact />
+        </>
+    )
+}
